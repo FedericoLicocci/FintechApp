82378ebfbef5733924312add2cced59ccfe0dca6
FedericoLicocci <federicolicocci@outlook.it>
2025-07-21 00:07:29 +0200

    Add Utente class

---
+++ src/main/java/com/example/demo/model/Utente.java
@@ -0,0 +1,61 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Utente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nome;
+    private String password;
+    private int saldo;
+
+    @OneToMany(mappedBy = "utente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Movement> movements;
+
+    public Utente() {}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Movement> getMovements() {
+        return movements;
+    }
+
+    public void setMovements(List<Movement> movements) {
+        this.movements = movements;
+    }
+}
